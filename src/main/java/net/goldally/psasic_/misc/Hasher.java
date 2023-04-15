0541422e4aec1f902961bf283a743caf17db3356
SilverCD <zatmonhp@yandex.ru>
2023-04-15 16:58:25 +0300

    Фикс хэш-суммы для генерации ключей.

--- src/main/java/net/goldally/psasic_/misc/Hasher.java
+++ src/main/java/net/goldally/psasic_/misc/Hasher.java
@@ -1,22 +1,24 @@
 package net.goldally.psasic_.misc;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.util.HexFormat;
 
 public class Hasher {
     private static MessageDigest md;
 
     static {
         try {
-            md = MessageDigest.getInstance("MD5");
+            md = MessageDigest.getInstance("SHA-256");
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static String hashed(String str) throws UnsupportedEncodingException {
-        byte[] bytesOfMessage = str.getBytes("UTF-8");
+        byte[] bytesOfMessage = str.getBytes(StandardCharsets.UTF_8);
         byte[] hash = md.digest(bytesOfMessage);
-        return hash.toString();
+        return HexFormat.of().formatHex(hash);
     }
 }
