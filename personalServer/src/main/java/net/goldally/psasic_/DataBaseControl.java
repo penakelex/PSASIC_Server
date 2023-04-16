5c322a1f04a765828f6155cc1cc362bf31cdfa98
SilverCD <zatmonhp@yandex.ru>
2023-04-16 16:03:39 +0300

    Определены SQL запросы

--- personalServer/src/main/java/net/goldally/psasic_/DataBaseControl.java
+++ personalServer/src/main/java/net/goldally/psasic_/DataBaseControl.java
@@ -13,9 +13,13 @@ public class DataBaseControl {
     public static PreparedStatement createGeoTableStatement;
     public static PreparedStatement createMessagesTableStatement;
     public static PreparedStatement createUsersTableStatement;
+
+
     public static PreparedStatement listUsersStatement;
     public static PreparedStatement isMemberStatement;
     public static PreparedStatement addMemberStatement;
+
+
     public static PreparedStatement isFriendsStatement;
     public static PreparedStatement isSentInvite;
     public static PreparedStatement setFriendStatement;
@@ -25,6 +29,8 @@ public class DataBaseControl {
     public static PreparedStatement listFriendsStatement;
     public static PreparedStatement setGeoStatement;
     public static PreparedStatement getGeoStatement;
+
+    //TODO: Сообщения
     public static PreparedStatement sendMessageStatement;
 
 
@@ -50,5 +56,15 @@ public class DataBaseControl {
         listUsersStatement = dbConnection.prepareStatement("SELECT * FROM users");
         isMemberStatement = dbConnection.prepareStatement("SELECT COUNT(*)>0 FROM users WHERE username=?");
         addMemberStatement = dbConnection.prepareStatement("INSERT INTO users (username) values (?)");
+
+        isFriendsStatement = dbConnection.prepareStatement("SELECT COUNT(*)>0 FROM friends WHERE friend1=? AND friend2=?");
+        isSentInvite = dbConnection.prepareStatement("SELECT COUNT(*)>0 FROM invites WHERE sender=? AND reciver=?");
+        setFriendStatement = dbConnection.prepareStatement("INSERT INTO friends (friend1, friend2) VALUES (?, ?)");
+        removeInviteStatement = dbConnection.prepareStatement("DELETE FROM invites WHERE sender=? AND reciver=?");
+        sendInviteStatement = dbConnection.prepareStatement("INSERT INTO invites (sender, reciver) VALUES (?, ?)");
+        removeFriendStatement = dbConnection.prepareStatement("DELETE FROM friends WHERE friend1=? AND friend2=?");
+        listFriendsStatement = dbConnection.prepareStatement("SELECT friend2 FROM friends WHERE friend1=?");
+        setGeoStatement = dbConnection.prepareStatement("UPDATE geo SET latitude=?, longitude=? WHERE user=?");
+        getGeoStatement = dbConnection.prepareStatement("SELECT latitude, longitude FROM geo WHERE user=?");
     }
 }
