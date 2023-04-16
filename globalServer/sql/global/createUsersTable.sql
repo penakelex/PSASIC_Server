6032017bc29cae4c472dedf0ea0a73c6cc18300a
SilverCD <zatmonhp@yandex.ru>
2023-04-16 17:29:50 +0300

    Временное решение: иконка по умолчанию

--- globalServer/sql/global/createUsersTable.sql
+++ globalServer/sql/global/createUsersTable.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS [users] (
+﻿CREATE TABLE IF NOT EXISTS [users] (
 [id] INTEGER  PRIMARY KEY NOT NULL,
 [username] TEXT  UNIQUE NOT NULL,
 [email] TEXT  UNIQUE NOT NULL,
 [name] TEXT DEFAULT 'Иван' NULL,
 [surname] TEXT DEFAULT 'Иванов' NULL,
-[icon] TEXT  NULL,
+[icon] TEXT DEFAULT 'https://raw.githubusercontent.com/penakelex/Hackathon_Project_Backend/main/default.jpg' NULL,
 [dateOfBirth] DATE  NULL,
 [password_hash] TEXT  NOT NULL
 )
